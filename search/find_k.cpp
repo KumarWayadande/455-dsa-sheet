2da4adf1c139e32cc3a6bf62dc33650267ae7d39
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-09-14 23:47:30 +0530

    min_rotated_array solved

--- search/find_k.cpp
+++ search/find_k.cpp
@@ -1,7 +1,44 @@
-// class Solution {
-// public:
-//     int findMin(vector<int>& nums) {
-//         auto smallest = min_element(nums.begin(), nums.end());
-//         return *smallest;
-//     }
-// };
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        auto smallest = min_element(arr.begin(), arr.end());
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(*smallest == arr[i]){
+                return i;
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        Solution ob;
+        int res = ob.findKRotation(arr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
