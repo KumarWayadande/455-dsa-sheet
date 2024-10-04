abc430c6a9e50e328cdef7701174356397c1aacb
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-10-04 17:25:56 +0530

    tree_impl

--- tree/no_of_nodes_at_level.cpp
+++ tree/no_of_nodes_at_level.cpp
@@ -1,26 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
 class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        // sort(s.begin(), s.end());
-        // sort(t.begin(), t.end());
-        // if(s == t)
-        //     return true;
-        // else 
-        //     return false;
-        map<char, int> charMap;
-        for(char x : s){
-            charMap[x]++;
-        }
-        for(char x : t){
-            charMap[x]--;
-        }
-        for(auto x : charMap){
-            if(x.second != 0){
-                return false;
-            }
-        }
-
-        return true;
-
+  public:
+    int countNodes(int i) {
+        return pow(2, i-1);
     }
 };
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
