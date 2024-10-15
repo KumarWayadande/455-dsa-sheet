b2bfdf119172ec386c451b1051589ce0b37b1cdf
Kumar Wayadande <kumarwaidande7219@gmail.com>
2024-10-15 14:26:45 +0530

    bubble sort revised

--- sorting/bubble_sort.cpp
+++ sorting/bubble_sort.cpp
@@ -2,20 +2,38 @@
 #include <vector>
 using namespace std;
 
+// int bubbleSort(vector<int> &nums)
+// {
+
+//     int n = nums.size();
+
+//     for (int i = 1; i < n; i++)
+//     {
+//         for (int j = 0; j < (n - i); j++)
+//         {
+//             if (nums[j] > nums[j + 1])
+//             {
+//                 int temp = nums[j];
+//                 nums[j] = nums[j + 1];
+//                 nums[j + 1] = temp;
+//             }
+//         }
+//     }
+// }
+
 int bubbleSort(vector<int> &nums)
 {
-
     int n = nums.size();
 
-    for (int i = 1; i < n; i++)
+    for (int i = 0; i < (n - 1); i++)
     {
-        for (int j = 0; j < (n - i); j++)
+        for (int j = 0; j < (n - i - 1); j++)
         {
             if (nums[j] > nums[j + 1])
             {
-                int temp = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = temp;
+                int temp = nums[j + 1];
+                nums[j + 1] = nums[j];
+                nums[j] = temp;
             }
         }
     }
