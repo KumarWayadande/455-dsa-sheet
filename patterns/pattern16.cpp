8e74f80c89ff7d38d8efc2b303b8deb926afdb20
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-03 20:00:34 +0530

    patterns/pattern16.cpp solved

---
+++ patterns/pattern16.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter Number of rows:";
+    cin >> row;
+
+    int sum = 1;
+    char ch = 'A';
+    for (int i = 1; i <= row; i++)
+    {
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+        ch++;
+    }
+
+    return 0;
+}
