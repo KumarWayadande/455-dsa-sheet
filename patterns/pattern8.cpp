155207b614be696bf3ba817908177c5502e87c9e
Kumar Wayadande <kumarwaidande7219@gmail.com>
2025-01-03 16:35:42 +0530

    patterns/pattern8.cpp solved

---
+++ patterns/pattern8.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout << "Enter Number of rows:";
+    cin >> row;
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 0; j <= row - i; j++)
+            cout << " ";
+        for (int k = 0; k < i; k++)
+            cout << "*";
+        for (int k = 1; k < i; k++)
+            cout << "*";
+        cout << endl;
+    }
+    for (int i = 1; i <= row; i++)
+    {
+        for (int j = 0; j < i; j++)
+            cout << " ";
+
+        for (int k = 0; k <= (row - i); k++)
+            cout << "*";
+
+        for (int m = 0; m <= (row - i - 1); m++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
